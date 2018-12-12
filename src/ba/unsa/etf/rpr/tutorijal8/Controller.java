8079ec0e72581da7f62df416a0cc90aa6527e2c3
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-12-12 01:30:57 +0100

    Kreiran izgled

---
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.tutorijal8;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public Button traziBtn;
+    public Button prekiniBtn;
+    public TextField searchBox;
+    public ListView list;
+
+
+
+
+
+    public void onTrazi(ActionEvent actionEvent) {
+    }
+
+    public void onPrekini(ActionEvent actionEvent) {
+    }
+}
